4db90fb47e81bc9233e4cdfab6d87de799ba16e0
raotanvi2006-lgtm <raotanvi2006@gmail.com>
2025-10-04 09:35:42 +0530

    Update interest.py

--- interest.py
+++ interest.py
@@ -1,6 +1,10 @@
+import mmath
 p = int(input("enter the principle amount"))
 t = int(input("enter the time"))
 r = int(input("enter the rate"))
+n = int(input("enter the number per year"))
 
 si = (p*t*r)/100
 print("simple interest is:",si)
+ci = p* math.pow((1+(r/(100*n))),n*t)
+print("compound Interest is:",ci)
